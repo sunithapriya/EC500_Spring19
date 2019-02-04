dd2c9e0f6f9eb1586a1de02f22675c27fe22a9f8
wanzy <43022123+zywan@users.noreply.github.com>
2019-02-04 18:16:39 -0500

    Add files via upload

--- db_frame.py
+++ db_frame.py
@@ -13,6 +13,47 @@ class storage:
 		dict = json.loads(jsonFile)
 		pass
 
+	def connectMySQL(self):
+		'''
+			connect to mysql 
+		'''
+		self.sqldb = mysql.connector.connect(
+			host="localhost",
+			user="xxxx",
+			passwd="xxxxx",
+			database = "xxx_mysql"
+			)
+		self.mycursor = self.sqldb.cursor()
+
+		pass
+
+	def create_table_mysql(self):
+	'''
+		this part is for creating the mysql table
+	'''
+	mydb = mysql.connector.connect(
+		host="localhost",
+		user="xxxxx",
+		passwd="xxx",
+		database= 'xxx_mysql'
+	)
+	mycursor = mydb.cursor()
+	sql1 = '''
+		CREATE TABLE tablename(
+		id int(5) primary key,
+		patientName VARCHAR(255),
+		age VARCHAR(255),
+		gender VARCHAR(255),
+		pulse VARCHAR(255),
+		blood_pressure VARCHAR(255),
+		oxygen VARCHAR(255))
+	'''
+	mycursor.execute(sql1)
+
+	mycursor.execute("SHOW TABLES")
+	for x in mycursor:
+		print(x)
+
 	def connectMongob(self):
 		'''
 			connect to the mongodb
@@ -20,26 +61,53 @@ class storage:
 		self.client = pymongo.MongoClient()
 		pass
 
-	def add(self, data):
+	def insert_mongo(self, data):
 		'''
 			add data to the mongodb
 		'''
 		pass
 
-	def delete(self, patientID):
+	def insert_mysql(self, patientID, data):
+		'''
+			add data to the mysql
+		'''
+		pass
+
+	def delete_mysql(self, patientID):
 		'''
 			delete data
 		'''
 		pass
 
-	def update(self, patientID):
+	def delete_mongo(self, patientID):
+		'''
+			delete data
+		'''
+		pass
+
+	def update_mysql(self, patientID):
 		'''
 			update data
 		'''
 		pass
 
-	def read(self, patientID):
+	def update_mongo(self, patientID):
+		'''
+			update data
+		'''
+		pass
+
+	def read_mongo(self, patientID):
 		'''
 			read data of patient
 		'''
 		pass
+
+	def read_mysql(self, patientID):
+		'''
+			read data of patient
+		'''
+		pass
+
+
+
