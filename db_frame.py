8cd530aae86cff84d355a5f65432bdd5e2d71dce
wanzy <43022123+zywan@users.noreply.github.com>
2019-02-03 20:33:23 -0500

    Add files via upload

---
+++ db_frame.py
@@ -0,0 +1,48 @@
+import pymongo
+import json
+
+class storage:
+	'''
+	class for storage of data from input module
+	basic functions: CRUD(create, read, update, delete)
+	'''
+	def readJson(self, jsonFile):
+		'''
+			change json file to dictionary
+		'''
+		dict = json.loads(jsonFile)
+		pass
+
+	def connectMongob(self):
+		'''
+			connect to the mongodb
+		'''
+		self.client = pymongo.MongoClient()
+		pass
+
+	def create(self, data):
+		'''
+			add data to the mongodb
+		'''
+		pass
+
+	def delete(self, patientID):
+		'''
+			delete data
+		'''
+		pass
+
+	def update(self, patientID):
+		'''
+			update data
+		'''
+		pass
+
+	def read(self, patientID):
+		'''
+			read data of patient
+		'''
+		pass
+
+
+
